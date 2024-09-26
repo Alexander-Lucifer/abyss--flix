f307506a3505f1011cad31bb0b06aec93188b8ff
Suryanshu Mittal <100358189+Alexander-Lucifer@users.noreply.github.com>
2024-09-26 15:09:59 +0530

    Home page changes

--- src/components/main/site-footer.tsx
+++ src/components/main/site-footer.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { siteConfig } from "@/configs/site";
-import Link from "next/link";
-import { buttonVariants } from "@/components/ui/button";
-import { Icons } from "@/components/icons";
+import React from 'react';
+import { siteConfig } from '@/configs/site';
+import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
+import { Icons } from '@/components/icons';
 
 const SiteFooter = () => {
   return (
@@ -15,13 +15,12 @@ const SiteFooter = () => {
                 <Link key={i} href={item.href} target="_blank" rel="noreferrer">
                   <div
                     className={buttonVariants({
-                      size: "sm",
-                      variant: "ghost",
+                      size: 'sm',
+                      variant: 'ghost',
                       className:
                         // "rounded-none text-neutral-700 hover:bg-transparent dark:text-neutral-50 dark:hover:bg-transparent",
-                        "rounded-none hover:bg-transparent",
-                    })}
-                  >
+                        'rounded-none hover:bg-transparent',
+                    })}>
                     {item.icon && <item.icon className="h-6 w-6" />}
                     <span className="sr-only">{item.title}</span>
                   </div>
@@ -35,37 +34,34 @@ const SiteFooter = () => {
               item.href && (
                 <li
                   key={i}
-                  className="text-xs text-foreground/60 hover:underline sm:text-sm"
-                >
+                  className="text-xs text-foreground/60 hover:underline sm:text-sm">
                   <Link href={item.href}>{item.title}</Link>
                 </li>
               ),
           )}
         </ul>
         <p className="text-xs text-foreground/60 sm:text-sm">
-          @ 2023-{new Date().getFullYear()} {siteConfig.author}.
+          @ 2024-{new Date().getFullYear() + 1} {siteConfig.author}.
         </p>
       </div>
       {/* <div className="container flex flex-col items-center justify-between md:h-24 md:flex-row md:py-0 gap-4"> */}
       <div className="container flex h-24 items-center gap-2 px-8">
         <Icons.play className="hidden h-6 w-6 md:block" />
         <p className="text-center text-xs leading-loose sm:text-sm md:text-left">
-          Built by{" "}
+          Built by{' '}
           <a
             href={siteConfig.links.twitter}
             target="_blank"
             rel="noreferrer"
-            className="font-medium underline underline-offset-4"
-          >
+            className="font-medium underline underline-offset-4">
             {siteConfig.author}
           </a>
-          . The source code is available on{" "}
+          . Follow me on{' '}
           <a
             href={siteConfig.links.github}
             target="_blank"
             rel="noreferrer"
-            className="font-medium underline underline-offset-4"
-          >
+            className="font-medium underline underline-offset-4">
             GitHub
           </a>
           .
