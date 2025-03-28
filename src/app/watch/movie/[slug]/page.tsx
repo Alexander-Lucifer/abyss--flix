0c119688f7e500f5c81e28a3cfa1dbe19814199c
Suryanshu Mittal <100358189+Alexander-Lucifer@users.noreply.github.com>
2025-03-28 22:41:52 +0530

    Api change Due to Faliure

--- src/app/watch/movie/[slug]/page.tsx
+++ src/app/watch/movie/[slug]/page.tsx
@@ -5,5 +5,5 @@ export const revalidate = 3600;
 
 export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
-  return <EmbedPlayer url={`https://vidsrc.dev/embed/movie/${id}`} />;
+  return <EmbedPlayer url={`https://vidsrc.cc/embed/movie/${id}`} />;
 }
