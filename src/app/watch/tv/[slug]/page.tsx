eb2e0e1c06823884d39bc3653954fe6aa935eadc
Suryanshu Mittal <100358189+Alexander-Lucifer@users.noreply.github.com>
2025-03-28 22:52:12 +0530

    version 2

--- src/app/watch/tv/[slug]/page.tsx
+++ src/app/watch/tv/[slug]/page.tsx
@@ -5,5 +5,5 @@ export const revalidate = 3600;
 
 export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
-  return <EmbedPlayer url={`https://vidsrc.cc/embed/tv/${id}`} />;
+  return <EmbedPlayer url={`https://vidsrc.cc/v2/embed/tv/${id}`} />;
 }
